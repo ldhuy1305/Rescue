55a794c0c74116dfbb00617714bf9ace27eb47ac
ldhuy1305 <bongmaxuyentuong1>
2024-03-01 15:45:00 +0700

    first commit

---
+++ WEB/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    }
+};
+</script>
+
+<style>
+@import 'bootstrap/dist/css/bootstrap.min.css';
+@import 'bootstrap';
+</style>
---
+++ WEB/src/main.js
@@ -0,0 +1,9 @@
+// Import the necessary packages
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// Create the app instance
+const app = createApp(App);
+
+// Mount the app to the DOM
+app.mount('#app');
