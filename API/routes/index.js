79f6d76f7e6d628df94e16c8f83250605b7c693d
ldhuydn1305 <bongmaxuyentuong1@gmail.com>
2024-06-10 09:09:41 +0700

    debug

--- API/routes/index.js
+++ API/routes/index.js
@@ -29,25 +29,28 @@ function route(app) {
     app.use("/api/v1/approval", approvalRoute);
     app.use("/api/v1/proof", proofRoute);
     app.use("/api/v1/help", helpRoute);
-    // const file = fs.readFileSync("/swagger.yaml", "utf8");
-    // const swaggerDocument = YAML.parse(file);
-    // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    fetch("/swagger.yaml")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.text();
-        })
-        .then((yamlText) => {
-            const swaggerDocument = YAML.parse(yamlText);
-            app.use(
-                "/api-docs",
-                swaggerUi.serve,
-                swaggerUi.setup(swaggerDocument),
-            );
-        })
-        .catch((error) => console.log("Error:", error));
+    if (process.env.NODE_ENV == "development") {
+        const file = fs.readFileSync("./swagger.yaml", "utf8");
+        const swaggerDocument = YAML.parse(file);
+        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    } else
+        fetch("/swagger.yaml")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.text();
+            })
+            .then((yamlText) => {
+                const swaggerDocument = YAML.parse(yamlText);
+                console.log(swaggerDocument);
+                app.use(
+                    "/api-docs",
+                    swaggerUi.serve,
+                    swaggerUi.setup(swaggerDocument),
+                );
+            })
+            .catch((error) => console.log("Error:", error));
 
     app.all("/*", (req, res, next) => {
         if (req.originalUrl === "/api-docs") {
